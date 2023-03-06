2610b0d32582a20e9c38dd0efd2eafa2949e4b81
YS Lu <bpenguin2027@gmail.com>
2023-03-06 23:30:43 +0800

    以ajax技術讀取json檔

---
+++ JsonWebApi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetJsonData()
+        {
+            string str_file = "~/App_Data/ODwsvTravelFood.json";
+            using (StreamReader sr = new StreamReader(Server.MapPath(str_file)))
+            {
+                var output = JsonConvert.DeserializeObject<List<ODwsvTravelFoodModel>>(sr.ReadToEnd());
+                return Json(output, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ JsonWebApi/Models/ODwsvTravelFoodModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+public class ODwsvTravelFoodModel
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    public string ID { get; set; }
+    /// <summary>
+    /// 名稱
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// 地址
+    /// </summary>
+    public string Address { get; set; }
+    /// <summary>
+    /// 電話
+    /// </summary>
+    public string Tel { get; set; }
+    /// <summary>
+    /// 主人的話
+    /// </summary>
+    public string HostWords { get; set; }
+    /// <summary>
+    /// 費用簡介
+    /// </summary>
+    public string Price { get; set; }
+    /// <summary>
+    /// 營業時間
+    /// </summary>
+    public string OpenHours { get; set; }
+    /// <summary>
+    /// 刷卡True_False
+    /// </summary>
+    public bool CreditCard { get; set; }
+    /// <summary>
+    /// 國民旅遊卡True_False
+    /// </summary>
+    public bool TravelCard { get; set; }
+    /// <summary>
+    /// 交通指引
+    /// </summary>
+    public string TrafficGuidelines { get; set; }
+    /// <summary>
+    /// 停車場
+    /// </summary>
+    public string ParkingLot { get; set; }
+    /// <summary>
+    /// 網站連結
+    /// </summary>
+    public string Url { get; set; }
+    /// <summary>
+    /// 業者email
+    /// </summary>
+    public string Email { get; set; }
+    /// <summary>
+    /// 部落格網址
+    /// </summary>
+    public string BlogUrl { get; set; }
+    /// <summary>
+    /// 寵物須知
+    /// </summary>
+    public string PetNotice { get; set; }
+    /// <summary>
+    /// 貼心叮嚀
+    /// </summary>
+    public string Reminder { get; set; }
+    /// <summary>
+    /// 最佳時令
+    /// </summary>
+    public string FoodMonths { get; set; }
+    /// <summary>
+    /// 容納人數
+    /// </summary>
+    public string FoodCapacity { get; set; }
+    /// <summary>
+    /// 美食特色
+    /// </summary>
+    public string FoodFeature { get; set; }
+    /// <summary>
+    /// 景點縣市
+    /// </summary>
+    public string City { get; set; }
+    /// <summary>
+    /// 鄉鎮
+    /// </summary>
+    public string Town { get; set; }
+    /// <summary>
+    /// 座標
+    /// </summary>
+    public string Coordinate { get; set; }
+    /// <summary>
+    /// 圖片
+    /// </summary>
+    public string PicURL { get; set; }
+}
